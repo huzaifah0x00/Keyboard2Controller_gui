a0d3baae6f68d4d9a26b1f3400ac5864d3297fe6
huzaifah0x00 <huzaifashaikh0012@gmail.com>
2018-11-28 22:32:17 +0500

    ...

--- server_gui.py
+++ server_gui.py
@@ -1,4 +1,7 @@
-import tkinter as tk
+from tkinter import Tk as Tk
+
+import tkinter.ttk as ttk
+
 import vjoy
 import pickle
 import socket
@@ -12,26 +15,26 @@ from threading import Thread
 class MainApplication:
     def __init__(self, master):
 
-        frame = tk.Frame(master, height=10, width=10, )
-        frame.grid(row=0,columnspan=2)
+        frame = ttk.Frame(master,)
+        frame.grid(row=0,columnspan=2,)
 
-        lower_frame = tk.Frame(master, height=10, width=10, )
-        lower_frame.grid(row=1, column=0)
+        lower_frame = ttk.Frame(master, height=10, width=10, )
+        lower_frame.grid(row=1, column=0, sticky="news")
 
 
         self.server_thread = Thread(target=self.StartServer)
 
-        self.start_server_btn = tk.Button(frame, text="Start Server", command=self.server_thread.start,
-                                          height=5, width=10)
-        self.start_server_btn.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.start_server_btn = ttk.Button(frame, text="Start Server", command=self.server_thread.start,)
+                                          # height=5, width=10)
+        self.start_server_btn.grid(row=0, column=0, sticky="news")
 
-        self.stop_server_btn = tk.Button(frame, text="Stop Server", command=self.StopServer, height=5,
-                                         width=10)
+        self.stop_server_btn = ttk.Button(frame, text="Stop Server", command=self.StopServer,)
+                                        # width=10, height=5)
         self.stop_server_btn.grid(row=0,column=1)
 
         self.server_status = "not_running"
 
-        self.server_status_TB = tk.Label(lower_frame, text="server status:"+self.server_status)
+        self.server_status_TB = ttk.Label(lower_frame, text="server status:"+self.server_status)
         self.server_status_TB.grid(row=2,column=0,)
 
 
@@ -40,8 +43,9 @@ class MainApplication:
         self.server_info = """
         hostname : {}
         port: {}
-        """.format(self.host, self.host)
-        self.server_info = 
+        """.format(self.host, self.port)
+        self.server_info_l = ttk.Label(lower_frame,text=self.server_info)
+        self.server_info_l.grid(row=2,column=1)
         self.update_widgets()
 
         self.clients = []
@@ -49,10 +53,10 @@ class MainApplication:
     def update_widgets(self):
         self.start_server_btn.configure(command=self.server_thread.start)
 
-        # self.server_status_TB.delete(1.0, tk.END)
-        # self.server_status_TB.insert(tk.END, self.server_status)
+        # self.server_status_TB.delete(1.0, ttk.END)
+        # self.server_status_TB.insert(ttk.END, self.server_status)
         self.server_status_TB.configure(text="server status: "+self.server_status)
-        self.server_info.configure(text = self.server_info)
+        self.server_info_l.configure(text = self.server_info)
 
 
     class Client(Thread):
@@ -136,6 +140,6 @@ class MainApplication:
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
+    root = Tk()
     main = MainApplication(root)
     root.mainloop()
